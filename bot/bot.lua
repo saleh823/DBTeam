87bcc72a08b5f9d60a5165c56d7bd9466f57b496
saleh823 <salehfffaaa@gmail.com>
2016-06-19 03:19:18 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,8 +256,8 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
-    admin_users = {},
+    sudo_users = {188383245},
+    admin_users = {188383245},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
